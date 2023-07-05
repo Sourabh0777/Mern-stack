f3ba0e7dc54dcdccf431103b30d9abf2393e8bb4
Sourabh <sourabh.verma9770@gmail.com>
2023-07-05 08:15:53 +0530

    w

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,25 +1,37 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import Backdrop from '../UIElements/Backdrop';
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
-  const toggleDrawerHandler = ()=>{setDrawerIsOpen(!drawerIsOpen)}  
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
+
   return (
     <React.Fragment>
-      {drawerIsOpen && <Backdrop onClick={toggleDrawerHandler}/>}
-      <SideDrawer show={drawerIsOpen} onClick={toggleDrawerHandler}>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
         <nav className="main-navigation__drawer-nav">
           <NavLinks />
         </nav>
-      </SideDrawer> 
+      </SideDrawer>
+
       <MainHeader>
-        <button className="main-navigation__menu-btn" onClick={toggleDrawerHandler}>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
           <span />
           <span />
           <span />
